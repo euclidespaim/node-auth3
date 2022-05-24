b696edc9dde622e45ac7c57f1ba889a4e2bf615d
euclidespaim <euclidespaim@gmail.com>
2022-05-24 18:36:45 -0300

    Políticas de acesso ao Conteúdo

--- src/middlewares/autorizacao.js
+++ src/middlewares/autorizacao.js
@@ -9,4 +9,4 @@ module.exports = (cargosObrigatorios) => (requisicao, resposta, proximo) => {
   proximo()
 }
 
-// Function composition is an approach where the result of one function is passed on to the next function, which is passed to another until the final function is executed for the final result.
+// Function composition is an approach where the result of one function is passed on to the next function, which is passed to another until the final function is executed for the final result...
