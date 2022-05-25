20cad5c309db5df910f56bdf550def5c60dde99f
euclidespaim <euclidespaim@gmail.com>
2022-05-25 11:11:58 -0300

    Políticas de acesso ao Conteúdo

--- src/middlewares/autorizacao.js
+++ src/middlewares/autorizacao.js
@@ -1,10 +1,43 @@
-module.exports = (cargosObrigatorios) => (requisicao, resposta, proximo) => {
-  if (cargosObrigatorios.indexOf(requisicao.user.cargo) === -1) {
+const controle = require('../controleDeAcesso')
+
+const metodos = {
+  ler: {
+    todos: 'readAny',
+    apenasSeu: 'readOwn'
+  },
+  criar: {
+    todos: 'createAny',
+    apenasSeu: 'createOwn'
+  },
+  remover: {
+    todos: 'deleteAny',
+    apenasSeu: 'deleteOwn'
+  }
+}
+
+module.exports = (entidade, acao) => (requisicao, resposta, proximo) => {
+  const permissoesDoCargo = controle.can(requisicao.user.cargo)
+  const acoes = metodos[acao]
+  const permissaoTodos = permissoesDoCargo[acoes.todos](entidade)
+  const permissaoApenasSeu = permissoesDoCargo[acoes.apenasSeu](entidade)
+
+  if (permissaoTodos.granted === false && permissaoApenasSeu.granted === false) {
     resposta.status(403).json({
       mensagem: 'Você não tem permissão para realizar esta ação.'
     })
     resposta.end()
     return
+  }
+
+  requisicao.acesso = {
+    todos: {
+      permitido: permissaoTodos.granted,
+      atributos: permissaoTodos.attributes
+    },
+    apenasSeu: {
+      permitido: permissaoApenasSeu.granted,
+      atributos: permissaoApenasSeu.attributes
+    }
   }
   proximo()
 }
