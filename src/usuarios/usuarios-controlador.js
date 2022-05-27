242d27048fea4a5c96793258030623080633091b
euclidespaim <euclidespaim@gmail.com>
2022-05-27 15:46:46 -0300

    Redefinir senha

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,85 +1,88 @@
-const Usuario = require('./usuarios-modelo')
-const { InvalidArgumentError, NaoEncontrado } = require('../erros')
+/* eslint-disable comma-dangle */
+const Usuario = require("./usuarios-modelo");
+const { InvalidArgumentError, NaoEncontrado } = require("../erros");
 
-const tokens = require('./tokens')
-const { EmailVerificacao, EmailRedefinicaoSenha } = require('./emails')
-const { ConversorUsuario } = require('../conversores')
+const tokens = require("./tokens");
+const { EmailVerificacao, EmailRedefinicaoSenha } = require("./emails");
+const { ConversorUsuario } = require("../conversores");
 
-function geraEndereco (rota, token) {
-  const baseURL = process.env.BASE_URL
-  return `${baseURL}${rota}${token}`
+function geraEndereco(rota, token) {
+  const baseURL = process.env.BASE_URL;
+  return `${baseURL}${rota}${token}`;
 }
 
 module.exports = {
-  async adiciona (req, res, proximo) {
-    const { nome, email, senha, cargo } = req.body
+  async adiciona(req, res, proximo) {
+    const { nome, email, senha, cargo } = req.body;
 
     try {
       const usuario = new Usuario({
         nome,
         email,
         emailVerificado: false,
-        cargo
-      })
-      await usuario.adicionaSenha(senha)
-      await usuario.adiciona()
+        cargo,
+      });
+      await usuario.adicionaSenha(senha);
+      await usuario.adiciona();
 
-      const token = tokens.verificacaoEmail.cria(usuario.id)
-      const endereco = geraEndereco('/usuario/verifica_email/', token)
-      const emailVerificacao = new EmailVerificacao(usuario, endereco)
-      emailVerificacao.enviaEmail().catch(console.log)
+      const token = tokens.verificacaoEmail.cria(usuario.id);
+      const endereco = geraEndereco("/usuario/verifica_email/", token);
+      const emailVerificacao = new EmailVerificacao(usuario, endereco);
+      emailVerificacao.enviaEmail().catch(console.log);
 
-      res.status(201).json()
+      res.status(201).json();
     } catch (erro) {
-      proximo(erro)
+      proximo(erro);
     }
   },
 
-  async login (req, res, proximo) {
+  async login(req, res, proximo) {
     try {
-      const accessToken = tokens.access.cria(req.user.id)
-      const refreshToken = await tokens.refresh.cria(req.user.id)
-      res.set('Authorization', accessToken)
-      res.status(200).json({ refreshToken })
+      const accessToken = tokens.access.cria(req.user.id);
+      const refreshToken = await tokens.refresh.cria(req.user.id);
+      res.set("Authorization", accessToken);
+      res.status(200).json({ refreshToken });
     } catch (erro) {
-      proximo(erro)
+      proximo(erro);
     }
   },
 
-  async logout (req, res, proximo) {
+  async logout(req, res, proximo) {
     try {
-      const token = req.token
-      await tokens.access.invalida(token)
-      res.status(204).json()
+      const token = req.token;
+      await tokens.access.invalida(token);
+      res.status(204).json();
     } catch (erro) {
-      proximo(erro)
+      proximo(erro);
     }
   },
 
-  async lista (req, res, proximo) {
+  async lista(req, res, proximo) {
     try {
-      const usuarios = await Usuario.lista()
+      const usuarios = await Usuario.lista();
       const conversor = new ConversorUsuario(
-        'json',
-        req.acesso.todos.permitido ? req.acesso.todos.atributos : req.acesso.apenasSeu.atributos
-      )
-      res.send(conversor.converter(usuarios))
+        "json",
+        req.acesso.todos.permitido
+          ? req.acesso.todos.atributos
+          : req.acesso.apenasSeu.atributos
+      );
+      res.send(conversor.converter(usuarios));
     } catch (erro) {
-      proximo(erro)
+      proximo(erro);
     }
   },
 
-  async verificaEmail (req, res, proximo) {
+  async verificaEmail(req, res, proximo) {
     try {
-      const usuario = req.user
-      await usuario.verificaEmail()
-      res.status(200).json()
+      const usuario = req.user;
+      await usuario.verificaEmail();
+      res.status(200).json();
     } catch (erro) {
-      proximo(erro)
+      proximo(erro);
     }
   },
 
-  async deleta (req, res, proximo) {
+  async deleta(req, res, proximo) {
     try {
       const usuario = await Usuario.buscaPorId(req.params.id)
       await usuario.deleta()
@@ -89,8 +92,11 @@ module.exports = {
     }
   },
 
-  async esqueciMinhaSenha (requisicao, resposta, proximo) {
-    const respostaPadrao = { mensagem: 'Se encontrarmos um usuário com este email, vamos enviar uma mensagem com as instruções para redefinir a senha' }
+  async esqueciMinhaSenha(requisicao, resposta, proximo) {
+    const respostaPadrao = {
+      mensagem:
+        "Se encontrarmos um usuário com este email, vamos enviar uma mensagem com as instruções para redefinir a senha!"
+    };
     try {
       const usuario = await Usuario.buscaPorEmail(requisicao.body.email)
       const token = await tokens.redefinicaoDeSenha.criarToken(usuario.id)
@@ -101,26 +107,31 @@ module.exports = {
     } catch (erro) {
       if (erro instanceof NaoEncontrado) {
         resposta.send(respostaPadrao)
-        return
+        return;
       }
 
-      proximo(erro)
+      proximo(erro);
     }
   },
 
-  async trocarSenha (requisicao, resposta, proximo) {
+  async trocarSenha(requisicao, resposta, proximo) {
     try {
-      if (typeof requisicao.body.token !== 'string' || requisicao.body.token.lenght === 0) {
-        throw new InvalidArgumentError('O token está inválido')
+      if (
+        typeof requisicao.body.token !== "string" ||
+        requisicao.body.token.lenght === 0
+      ) {
+        throw new InvalidArgumentError("O token está inválido");
       }
 
-      const id = await tokens.redefinicaoDeSenha.verifica(requisicao.body.token)
-      const usuario = await Usuario.buscaPorId(id)
-      await usuario.adicionaSenha(requisicao.body.senha)
-      await usuario.atualizarSenha()
-      resposta.send({ mensagem: 'Sua senha foi atualizada com sucesso' })
+      const id = await tokens.redefinicaoDeSenha.verifica(
+        requisicao.body.token
+      );
+      const usuario = await Usuario.buscaPorId(id);
+      await usuario.adicionaSenha(requisicao.body.senha);
+      await usuario.atualizarSenha();
+      resposta.send({ mensagem: "Sua senha foi atualizada com sucesso" });
     } catch (erro) {
-      proximo(erro)
+      proximo(erro);
     }
-  }
-}
+  },
+};
